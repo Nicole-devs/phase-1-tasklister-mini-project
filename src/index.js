aed397123919456d5b1c35f948262524bc984b78
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-03-24 08:40:21 +0300

    mini project P1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    let input =  document.getElementById('new-task-description').value
+    console.log(input)
+
+    let todo = document.getElementById('tasks')
+    let task = document.createElement('p')
+    let delTsk = document.createElement('button')
+    task.textContent = input
+    delTsk.textContent = 'x'
+    delTsk.style.marginLeft = '20px'
+    delTsk.style.background ='red'
+    delTsk.style.color = 'black'
+  
+    todo.appendChild(task)
+    task.appendChild(delTsk)
+
+    delTsk.addEventListener('mousedown', ()=>{
+      delTsk.parentNode.remove()
+    })
+
+  })
+  
   // your code here
 });
